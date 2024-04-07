b753571982785cff586c4a318ea639bd061feb64
pannet1 <prog@ecomsense.in>
2024-04-07 23:27:06 +0530

    add universe.csv to gitignore

--- ranga_breakout/__init__.py
+++ ranga_breakout/__init__.py
@@ -1,11 +1,11 @@
 from toolkit.logger import Logger
 from toolkit.fileutils import Fileutils
 
-logging = Logger(10)
+S_DATA = "../data/"
+logging = Logger(10, S_DATA + "log.txt")
 O_FUTL = Fileutils()
 
 
-S_DATA = "../data/"
 S_DUMP = S_DATA + "symbols.json"
 S_UNIV = S_DATA + "universe.csv"
 S_OUT = S_DATA + "out.csv"
